f0cc035a89affa45eee63a64418f9a50b5db7f5d
Jasmine Fahmy <cds.GasminMohamed24128@alexu.edu.eg>
2026-09-03 18:48:25 +0300

    API
    Removed the prefix 'AQ.' from the API key.

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ import os
 from streamlit_option_menu import option_menu 
 
 # --- CONFIGURATION ---
-API_KEY = "AQ.Ab8RN6K2RTZ1kc8J4r3RcRb5L7BGvkQlsMcn1gyRTipXp2ZC8A"
+API_KEY = "Ab8RN6K2RTZ1kc8J4r3RcRb5L7BGvkQlsMcn1gyRTipXp2ZC8A"
 API_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-09-2025:generateContent?key={API_KEY}"
 
 MEDICAL_PROMPT = """
